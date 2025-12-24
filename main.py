dae55ea11623cb3d723eaa85a3e1846466c5f168
Datta <datta.yelisetty93@gmail.com>
2025-12-24 07:04:54 +0530

    Hello World Chain

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from dotenv import load_dotenv
-import os
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
 def main():
-    print("Hello from langchain-course!")
-    
+    print("Hello from langchain-course!")    
+    information = """
+Elon Reeve Musk[b] (born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, X, and xAI. Musk has been the wealthiest person in the world since 2021; as of December 2025, Forbes estimates his net worth to be around $684 billion.
 
+Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania in Philadelphia, United States, before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002."""
+    summary_template = """
+    Given the information {information} about a person, I want you to create
+    1. A short summary about the person.
+    2. 2 interesting facts about the person."""
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template
+    )
+
+    # llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    llm = ChatOllama(model="gemma3:270m", temperature=0)
+    chain = summary_prompt_template | llm
+    response = chain.invoke(input={"information": information})
+    print(response.content)
 
 
 if __name__ == "__main__":
